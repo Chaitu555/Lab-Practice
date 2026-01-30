0e80949bfc3104ab144007f69aca8f4899bfeec7
Chaitu555 <110188605+Chaitu555@users.noreply.github.com>
2026-01-30 17:44:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get('/')
 def r(): return {'Hello':'Docker'}
+
+from fastapi import FastAPI
+app=FastAPI()
+@app.get('/')
+def r(): return {'Hello':'Git'}
