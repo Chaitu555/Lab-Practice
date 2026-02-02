2c6ada6a717892d197849ba514d0743d76abfb5a
Chaitu555 <110188605+Chaitu555@users.noreply.github.com>
2026-02-02 18:38:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,3 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get('/')
 def r(): return {'Hello':'Git'}
-
-from fastapi import FastAPI
-app=FastAPI()
-@app.get('/')
-def r(): return ('Hello':'Terraform')
