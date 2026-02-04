b6ed0e13417c1ec6d09e44ff494366a6a2cfcaf9
Chaitu555 <110188605+Chaitu555@users.noreply.github.com>
2026-02-04 19:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,3 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get('/')
 def r(): return {'Hello':'Git'}
-
-from fastapi import FatAPI
-app=FastAPI()
-@app.get('/')
-def r(): return {'Hello':'K8s'}
