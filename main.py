19c92afbe21715fe76010cc294537d7b6887deb0
Chaitu555 <110188605+Chaitu555@users.noreply.github.com>
2026-02-25 18:28:41 +0530

    Clean up main.py and document SELinux setup
    Removed unused print statements and added comments for SELinux installation and SSH service restart.

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ def r(): return {'Hello':'K8s'}
 age = 25,
 print(age) 
 
-"Hello":"World"
-age = 25,
-print(age)
-
 pwd, cd, cd., cd .., ls, ls -lt, ls -lrt, mkdir
 ssh john@jump_host_company.com
 password yes
@@ -28,4 +24,20 @@ sudo groupadd Family
 sudo useradd chaitu
 sudo usermod -aG Family chaitu
 
+# Restarting the sshd helps to implement the changes done in the script
 sudo systemctl restart sshd
+
+# For installing the SELinux on the server with the multiple packages
+sudo yum install -y selinux-policy selinux-policy-targeted policycoreutils
+#sudo  --------------------- for running the cmd with root access
+# yum  --------------------- package manager used for RHEL,CentOS,Rocky Linux, AlmaLinux
+#-y    --------------------- For executig the yes permission without any extra permission
+#selinux-policy ------------ It contains core security rules and defenetions
+#selinux-policy-targeted  --- commonly used policy which contains multiple policies 
+#a) targeted (default)
+#b) strict (very restrictive)
+#c) mls (military-level security)
+#Policycoreutils ------------Tools to manage SELinux.
+#For example:getenforce; setenforce; restorecon; semanage
+
+
