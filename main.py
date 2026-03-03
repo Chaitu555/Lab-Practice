8fc7b29f44a5bd51836d3713ad0e14e97f9c4914
Chaitu555 <110188605+Chaitu555@users.noreply.github.com>
2026-03-03 16:46:30 +0530

    Refactor FastAPI app to remove duplicates
    Removed duplicate FastAPI app definitions and adjusted response.

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ app=FastAPI()
 @app.get('/')
 def r(): return {'Hello':'Docker'}
 
-from fastapi import FastAPI
-app=FastAPI()
-@app.get('/')
-def r(): return {'Hello':'Git'}
-
-from fastapi i port FastAPI
-app=FastAPI()
-@app.get('/')
-def r(): return {'Hello':'K8s'}
-
 "Hello: World"
 age = 25,
 print(age) 
